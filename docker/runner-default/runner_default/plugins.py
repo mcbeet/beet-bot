3df9e335f599a30a63b608d9692ca7ea6b74dbe9
Valentin Berlier <berlier.v@gmail.com>
2023-08-04 01:49:56 +0200

    chore: precompile utils

--- docker/runner-default/runner_default/plugins.py
+++ docker/runner-default/runner_default/plugins.py
@@ -6,6 +6,7 @@ def precompile_ast(ctx: Context):
     ctx.require(
         "bolt",
         "bolt_expressions",
+        "beet_plugins.utils",
         "wicked_expressions",
         "mecha",
     )
